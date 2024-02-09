f90ee577d3c62e764da724f2c1d2782e011e2b44
EnzoPierucci <enzopierucci@gmail.com>
2024-02-09 19:05:59 +0100

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <script>
+import Header from "./components/Header.vue"
 
+export default {
+    components: {
+        Header,
+    }
+}
 </script>
 
 <template>
-  
+<Header></Header>
+ 
 </template>
 
 <style scoped>
